28511a6aaacca10b27a1497f2193ebba710d1f9a
Rustem Mukazhanov <r.mukazhanov@gmail.com>
2021-12-23 23:57:46 +0600

    first changes to items

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoList from "./Todo/TodoList";
 
 function App() {
+  const todos = [
+    { id: 1, completed: false, title: "Buy some groceries" },
+    { id: 2, completed: false, title: "Check the mailbox" },
+    { id: 3, completed: false, title: "Pay for bills" },
+    { id: 4, completed: false, title: "Clean the fridge" },
+    { id: 5, completed: false, title: "Take the dog for a walk" },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>React Tutorial</h1>
+      <TodoList todos={todos} />
     </div>
   );
 }
